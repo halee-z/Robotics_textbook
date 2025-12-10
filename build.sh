1e309054b9b7a0b7bd7d7b2cf3237c8371014288
halee-z <halee.sadia15@gmail.com>
2025-12-10 06:12:32 +0500

    Remove unnecessary build.sh file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Ensure proper permissions for docusaurus binary
-chmod +x node_modules/.bin/docusaurus || echo "Permission setting not needed or failed"
-
-# Run the build command
-npx docusaurus build
